82217950b759c24c5ae32ee88e2ea47d9ce09026
BasanttH <90738027+BasanttH@users.noreply.github.com>
2024-12-15 11:35:42 -0500

    initial commit

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 import styles from './ProjectsStyles.module.css';
 import lemon from '../../assets/Logo Footer.2c859235d6c57bb54176.png';
 import Watering_sys from '../../assets/Auto.png';
-import hipsster from '../../assets/hipsster.png';
+import TemSystem from '../../assets/img2.png';
 import Audio_FLT from '../../assets/Proj3.png';
 import ProjectCard from '../../common/ProjectCard';
 
@@ -14,7 +14,7 @@ function Projects() {
           src={lemon}
           link="https://github.com/BasanttH/Frontened-capstone-LittileLemon.git"
           h3="Little Lemon - Coursera"
-          p="Booking table form "
+          p="Website - booking table form "
         />
         <ProjectCard
           src={Watering_sys}
@@ -22,18 +22,25 @@ function Projects() {
           h3="Auto watering system "
           p="Plant watering system with Arduino"
         />
-        <ProjectCard
-          src={hipsster}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Hipsster"
-          p="Glasses Shop"
-        />
-        <ProjectCard
+          <ProjectCard
           src={Audio_FLT}
           link="https://acrobat.adobe.com/id/urn:aaid:sc:US:acf43954-379d-4b51-8cea-525c72cc61cf"
-          h3="Audio filter"
+          h3="Audio Filter"
           p="Digital Filters,Noise Removal"
         />
+        <ProjectCard
+          src={TemSystem}
+          link="https://www.youtube.com/watch?v=CpE3X7Xkbzw"
+          h3="Temperature Alert System"
+          p="Temperature alert system with audible alarms"
+        />
+         <ProjectCard
+          src={TemSystem}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Hair day website"
+          p="Hair day salon website - HTML,CSS"
+        />
+      
       </div>
     </section>
   );
