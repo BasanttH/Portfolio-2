4987375f3504212dcc758d3f0cea4dae93dd5297
BasanttH <90738027+BasanttH@users.noreply.github.com>
2024-12-16 22:14:31 -0500

    commit

--- src/sections/Profile/Profile.jsx
+++ src/sections/Profile/Profile.jsx
@@ -6,7 +6,7 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/BasantH.pdf';
+import CV from '../../assets/Basant_H.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function Profile() {
